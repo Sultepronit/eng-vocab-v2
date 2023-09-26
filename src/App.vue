65cdfa8832f7492388b9022f4f09dfe9146ac1bd
Sultepronit <sultepronit@gmail.com>
2023-09-26 11:39:29 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import getData from '@/getdb.js';
+export default {
+  name: 'App',
+  data() {
+    return {
+      db: [],
+      index: 0,
+      kanji: ''
+    }
+  },
+  methods: {
+    kanji2() {
+      this.kanji = this.db[this.index++][0];
+    }
+  },
+  created() {
+    console.timeLog('tt', 'created!');
+    getData('kanji', 'A', 'D').then(response => {
+      this.db = response;
+      this.kanji2();
+      console.timeLog('tt', 'received data!');
+    });
+  },
+}
+  
+</script>
+
+<template>
+  <button @click="kanji2">next</button>
+  <!-- <h1>{{ db[index][0] }}</h1> -->
+  <h1>{{ kanji }}</h1>
+  <p v-for="card in db">{{ card }}</p>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+/* import './assets/main.css' */
+console.time('tt');
+console.log('start!');
+console.timeLog('tt', 'main.js');
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app');
