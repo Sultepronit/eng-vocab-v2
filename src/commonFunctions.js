073bf40abeb2bc36ef25c80a1ee85bc0fe2a013d
Sultepronit <sultepronit@gmail.com>
2023-09-27 12:18:58 +0300

    trying implementing pronunciation

--- src/commonFunctions.js
+++ src/commonFunctions.js
@@ -1,20 +1,7 @@
-/* export function randomFromRange(from, to) {
-    return Math.round((Math.random() * to) - from);
-}
-//const arr1 = [0, 1, 2, 3, 4, 5, 6];
-export function pullRandomFromArray(array) {
-    const index = randomFromRange(0, array.length - 1);
-    const item = array[index];
-    array.splice(index, 1);
-    return item;
-} */
-//console.log(pullRandomFromArray(arr1));
-//console.log(arr1);
-
 function randomFromRange(from, to) {
-    return Math.round((Math.random() * to) - from);
+    return Math.round((Math.random() * (to - from)) + from);
 }
-//const arr1 = [0, 1, 2, 3, 4, 5, 6];
+
 function pullRandomFromArray(array) {
     const index = randomFromRange(0, array.length - 1);
     const item = array[index];
--- src/nextCard.js
+++ src/nextCard.js
@@ -1,20 +1,24 @@
 import cf from '@/commonFunctions';
+import soundObject from '@/soundObject';
+const audio = new Audio();
 
 function nextCard({ cards, content, learnList, confirmList, repeatList }) {
     const cardType = cf.pullRandomFromArray(content);
     console.log(cardType);
     console.log(content);
 
-   /*  let currentCardId = 0;
-    if(cardType === 'LEARN') {
-        currentCardId = cf.pullRandomFromArray(learnList);
-    } */
     const currentCardId = cardType === 'LEARN' ? cf.pullRandomFromArray(learnList)
         : cardType === 'CONFRIM' ? cf.pullRandomFromArray(confirmList)
             : cf.pullRandomFromArray(repeatList);
+
     console.log(currentCardId);
     const currentCard = cards[currentCardId];
     console.log(currentCard);
+
+    const urls = soundObject[currentCard.word.toLowerCase()];
+    console.log(urls);
+    audio.src = urls[0];
+    audio.play();
 
     return {cardType, cardId: currentCardId, card: currentCard };
 }
