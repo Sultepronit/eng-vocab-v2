58da478e63bf071735d9dc8f158e58c65cb02dd4
Sultepronit <sultepronit@gmail.com>
2023-10-20 18:41:53 +0300

    added functionality; now we can flip through the cards

--- src/desktop/DesktopApp.vue
+++ src/desktop/DesktopApp.vue
@@ -2,14 +2,14 @@
 import { ref, onMounted, watch } from 'vue';
 import startSession from './startSession';
 import nextCard from './nextCard';
-import { connectKeyControl } from './keyboardListener';
+//import { connectKeyControl } from './keyboardListener';
 import { connectPlayback, preparePlaylist, startSpeaking } from '@/pronunciation';
 
 
 const theInput = ref(null);
 
 //const control = reactive(action);
-const keyControl = ref({
+/* const keyControl = ref({
     monitor: '_',
     actionName: '',
     play: false,
@@ -17,28 +17,8 @@ const keyControl = ref({
     answer: false,
     evaluate: false,
     train: false
-});
-connectKeyControl(keyControl);
-
-/* watch(() => keyControl.value.monitor, (newVal) => {
-    console.log(newVal);
-    console.log(keyControl.value.monitor);
 }); */
-//console.log(keyControl);
-//setInterval(() => {console.log(control.monitor)}, 2000);
-//const actionMonitor = ref('');
-/* function getKey(key) {
-    console.log(key);
-    actionMonitor.value = key;
-    console.log(theInput.value.value);
-    console.log(theInput.value);
-    console.log(theInput.value.innerHTNL);
-    console.log(theInput.value.outerHTML);
-}
-document.addEventListener('keyup', (event) => getKey(event.key));
-//document.addEventListener('keypress', (event) => console.log(event)) */
-
-//const theInput = ref(null);
+//connectKeyControl(keyControl);
 
 const word = ref('word');
 const transc = ref('transcritption');
@@ -57,14 +37,16 @@ onMounted(() => {
     console.timeLog('tt', 'ready to go!');
 });
 
+const mark = ref('');
+const expectedAction = ref('');
 
 function showNext() {
-    keyControl.value.actionName = 'showAnswer';
     theInput.value.focus();
-    //console.log(theInput);
-    //console.log(session.value)
+
     current.value = nextCard(session);
     console.log(current.value);
+
+    mark.value = '';
 
     word.value = current.value.direction === 'FORWARD'
         ? current.value.word.question : current.value.word.hint;
@@ -75,27 +57,106 @@ function showNext() {
         ? current.value.card.transl : '';
 
     example.value = '';
+
+    preparePlaylist(current.value.word.variants);
+    expectedAction.value = 'initShowAnswer';
 }
 
-watch(() => keyControl.value.showAnswer, (nv) => {
-    if(nv) {
-        keyControl.value.showAnswer = false;
-        console.log('show!');
-    }
-});
+function play() {
+    //this.playback.on = true;
+    startSpeaking();
+}
 
+function showAnswer() {
+    console.log('show!');
+
+    transc.value = current.value.card.transc;
+    word.value = current.value.word.answer;
+    if(current.value.direction === 'FORWARD') {
+        transl.value = current.value.card.transl;
+    } 
+    example.value = current.value.card.example;
+
+    try {
+        play();
+    } catch (error) {
+        alert(error);
+    }
+
+    expectedAction.value = 'evaluate';
+}
+
+function saveProgress() {
+    console.log('save!');
+    showNext();
+}
+
+const keyMonitor = ref('_');
+const actions = {
+    initShowAnswer(key) {
+        if(key === 'Enter') showAnswer();
+    },
+    evaluate(key) {
+        if(key === 'g') {
+            mark.value = 'good';
+            return;
+        }
+        if(key === 'b') {
+            mark.value = 'bad';
+            return;
+        }
+        if(key === 'n') {
+            mark.value = 'neutral';
+            return;
+        }
+        if(key === 'Enter' && mark.value !== '') {
+            saveProgress();
+        }
+    }
+};
+function keyAction(key) {
+    //console.log(key);
+    if(key === 'Shift') return;
+
+    keyMonitor.value = key;
+    actions[expectedAction.value](key);
+    //keyControl.value.monitor = key;
+    //actions[keyControl.value.actionName](key);
+   /*  actionMonitor.value = key;
+    console.log(theInput.value.value);
+    console.log(theInput.value);
+    console.log(theInput.value.innerHTNL);
+    console.log(theInput.value.outerHTML); */
+}
+document.addEventListener('keyup', (event) => keyAction(event.key));
 
 </script>
 <template>
     <h1>This is a desktop version!</h1>
-    <!-- <p>{{ actionMonitor }}</p> -->
-    <p>{{ keyControl.monitor }}</p>
+    <p>{{ keyMonitor }}</p>
+    <!-- <p>{{ keyControl.monitor }}</p> -->
     <input ref="theInput" type="text" />
 
-    <main>
+    <main v-bind:class="mark">
         <p class="word" v-html="word" />
         <p class="transc">{{ transc }}</p>
         <p class="transl" v-html="transl" />
         <p class="example" v-html="example" />
     </main>
 </template>
+
+<style>
+main {
+    border: 5px solid white;
+}
+.good {
+    /* border: 4px solid green; */
+    border-color: green;
+}
+.bad {
+    border-color: red;
+}
+.neutral {
+    border-color: yellow;
+}
+</style>
